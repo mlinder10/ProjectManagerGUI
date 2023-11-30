58d33fbcbc4a3de07d165b4d277284aaa91e928c
Matt Linder <linder2015@outlook.com>
2023-11-30 13:13:48 -0500

    file organization

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -18,6 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("Dashboard"), 640, 480);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
