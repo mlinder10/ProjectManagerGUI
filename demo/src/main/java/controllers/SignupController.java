15c827b836ea9a7c05cc22f304dd31519e44846f
Alexis-Hill6 <aah6@email.sc.edu>
2023-11-30 14:35:18 -0500

    more stuff in signup controller

--- demo/src/main/java/controllers/SignupController.java
+++ demo/src/main/java/controllers/SignupController.java
@@ -6,13 +6,29 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.TextField;
 import com.example.App;
 import models.*;
 
 import javafx.fxml.Initializable;
 
 public class SignupController implements Initializable {
+
+    @FXML
+    private TextField txt_username;
+    @FXML
+    private TextField txt_password;
+    @FXML
+    private TextField txt_email;
+    @FXML
+    private Label lbl_error;
+
+    @FXML
+    private void btnRegistarClicked(MouseEvent event) throws IOException {
+
+        String username
+    }
+
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
