86db7e433da6338bca223127aa5486d592aa1946
Matt Linder <linder2015@outlook.com>
2023-12-05 12:06:50 -0500

    fixing minor errors

--- demo/src/main/java/controllers/DashboardController.java
+++ demo/src/main/java/controllers/DashboardController.java
@@ -1,23 +1,16 @@
 package controllers;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.example.App;
 
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import models.Project;
 import models.ProjectFACADE;
 import javafx.fxml.Initializable;
 
-public class DashboardController implements Initializable{
+public class DashboardController implements Initializable {
     private ProjectFACADE facade;
 
     private Button Project1;
@@ -33,34 +26,30 @@ public class DashboardController implements Initializable{
         facade.getProject("Project 1");
         facade.openProject("Project 1");
         try {
-             App.setRoot("OwnerProject");
+            App.setRoot("OwnerProject");
         } catch (Exception e) {
             e.printStackTrace();
         }
-       
 
-           
     }
 
     private void handleTask1ButtonClick(MouseEvent event) {
-        ProjectFACADE facade =ProjectFACADE.getInstance();
+        ProjectFACADE facade = ProjectFACADE.getInstance();
 
         facade.getTask("Task 1");
         try {
-             App.setRoot("task1");
+            App.setRoot("task1");
         } catch (Exception e) {
             e.printStackTrace();
         }
-       
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-      facade = ProjectFACADE.getInstance();
+        facade = ProjectFACADE.getInstance();
         Project1.setOnMouseClicked(event -> handleProject1ButtonClick(event));
         Task1.setOnMouseClicked(event -> handleTask1ButtonClick(event));
     }
 
-    
-    
 }
--- demo/src/main/java/controllers/ProjectController.java
+++ demo/src/main/java/controllers/ProjectController.java
@@ -1,22 +1,12 @@
 package controllers;
-import java.io.IOException;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.example.App;
-
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import models.Project;
-import models.ProjectFACADE;
 import javafx.fxml.Initializable;
 
-public class ProjectController implements Initializable{
+public class ProjectController implements Initializable {
 
     private Button Task1;
     private Button Dashboard;
@@ -25,12 +15,11 @@ public class ProjectController implements Initializable{
     private Button addTask;
     private Button addSection;
     private Button Comments;
-    
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'initialize'");
     }
-    
+
 }
